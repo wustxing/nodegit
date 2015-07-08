696b094bfcbdc0b3946d1556f04c8a4163470977
wustxing <403860586@qq.com>
2015-07-08 17:55:06 +0800

    唉

--- test.js
+++ test.js
@@ -1,1 +1,16 @@
-fdfdfd
+var mysql      = require('node/mysql');
+var connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : '000000'
+});
+
+connection.connect();
+
+connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
+    if (err) throw err;
+
+    console.log('The solution is: ', rows[0].solution);
+});
+
+connection.end();
